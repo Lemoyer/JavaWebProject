0c9603f852ea9201702493c5831987f6bb96c37a
liyuh <liyuhang_30@163.com>
2024-07-07 21:53:50 +0800

    增加课程信息查询接口，实现类和对应的测试类，在测试类中debug测试成功

---
+++ JavaWeb-content/JavaWeb-content-service/src/main/java/com/javaweb/content/service/CourseBaseInfoService.java
@@ -0,0 +1,13 @@
+package com.javaweb.content.service;
+
+import com.javaweb.base.model.PageParams;
+import com.javaweb.base.model.PageResult;
+import com.javaweb.content.model.dto.QueryCourseParamsDto;
+import com.javaweb.content.model.po.CourseBase;
+/**
+ * 课程基本信息管理业务接口
+**/
+public interface CourseBaseInfoService  {
+    PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
+
+ }
---
+++ JavaWeb-content/JavaWeb-content-service/src/main/java/com/javaweb/content/service/impl/CourseBaseInfoServiceImpl.java
@@ -0,0 +1,39 @@
+package com.javaweb.content.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.javaweb.base.model.PageParams;
+import com.javaweb.base.model.PageResult;
+import com.javaweb.content.mapper.CourseBaseMapper;
+import com.javaweb.content.model.dto.QueryCourseParamsDto;
+import com.javaweb.content.model.po.CourseBase;
+import com.javaweb.content.service.CourseBaseInfoService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CourseBaseInfoServiceImpl implements CourseBaseInfoService {
+   @Autowired
+   CourseBaseMapper courseBaseMapper;
+
+    @Override
+    public PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto) {
+        LambdaQueryWrapper<CourseBase> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotEmpty(queryCourseParamsDto.getCourseName()),CourseBase::getName,queryCourseParamsDto.getCourseName());
+        queryWrapper.eq(StringUtils.isNotEmpty(queryCourseParamsDto.getAuditStatus()),CourseBase::getAuditStatus,queryCourseParamsDto.getAuditStatus());
+        Page<CourseBase> page = new Page<>(pageParams.getPageNo(), pageParams.getPageSize());
+        // 查询数据内容获得结果
+        Page<CourseBase> pageResult = courseBaseMapper.selectPage(page, queryWrapper);
+        // 获取数据列表
+        List<CourseBase> list = pageResult.getRecords();
+        // 获取数据总数
+        long total = pageResult.getTotal();
+        // 构建结果集
+        PageResult<CourseBase> theResult = new PageResult<>(list, total, pageParams.getPageNo(), pageParams.getPageSize());
+        return theResult;
+    }
+}
