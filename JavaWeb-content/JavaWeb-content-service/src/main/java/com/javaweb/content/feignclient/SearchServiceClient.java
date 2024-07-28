95dc1d9fbc2bbe59eaf29975b06ab0b22df5dacc
liyuh <liyuhang_30@163.com>
2024-07-28 12:03:47 +0800

    课程搜索测试成功，elasticsearch索引添加成功，测试成功

---
+++ JavaWeb-content/JavaWeb-content-service/src/main/java/com/javaweb/content/feignclient/SearchServiceClient.java
@@ -0,0 +1,12 @@
+package com.javaweb.content.feignclient;
+import com.javaweb.content.model.dto.CourseIndex;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(value = "search",fallbackFactory = SearchServiceClientFallbackFactory.class)
+public interface SearchServiceClient {
+
+    @PostMapping("/search/index/course")
+    public Boolean add(@RequestBody CourseIndex courseIndex);
+}
